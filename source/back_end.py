30a94516101012a3634f0718198331f8bdde56a0
Leozao-silva <leonardo_felipe_silva@outlook.com>
2023-07-02 12:46:21 -0300

    commited back end
    Improved the methods bellows.
    - adicionaMaoObra
    - editarMaoObra
    - gerarRelatorio

--- source/back_end.py
+++ source/back_end.py
@@ -26,12 +26,43 @@ class User:
         with open(MATERIAIS, 'w') as file:
             json.dump(relatoriomp, file, indent=4)
 
-    def adicionaMaoObra(self):
-        pass
-    def editarMaoObra(self):
-        pass
+    def adicionaMaoObra(self, desc, val):
+        with open(MAODEOBRA, 'r') as file:
+            relatoriomo = json.load(file)
+
+        relatoriomo["index"].append(len(relatoriomo['index'])+1)
+        relatoriomo["descricao"].append(desc)
+        relatoriomo["valor"].append(val)
+
+        with open(MAODEOBRA, 'w') as file:
+            json.dump(relatoriomo, file, indent=4)
+    
+    def editarMaoObra(self, ind, desc, val):
+        with open(MAODEOBRA, 'r') as file:
+            relatoriomo =json.load(file)
+        
+        relatoriomo["descricao"][ind-1] = desc
+        relatoriomo["valor"][ind-1] = val
+
+        with open(MAODEOBRA, 'w') as file:
+            json.dump(relatoriomo, file, indent=4)
+
     def gerarRelatorio(self):
-        pass
+        with open(MATERIAIS, 'w') as file:
+            relatoriomp = json.load(file)
+        
+        relatoriomp_formatado = ''
+        for iten in range(0, len(relatoriomp['index'])):
+            relatoriomp_formatado += ('{}{}R${}\n}'.format(relatoriomp['index'][iten], relatoriomp['descricao'][iten], relatoriomp['valor'][iten]))
+
+        with open(MAODEOBRA, 'w') as file:
+            relatoriomo = json.load(file)
+        
+        relatoriomo_formatado = ''
+        for iten in range(0, len(relatoriomo['index'])):
+            relatoriomo_formatado += ('{}{}R${}\n}'.format(relatoriomo['index'][iten], relatoriomo['descricao'][iten], relatoriomo['valor'][iten]))
+
+
     def salvarRelatorio(self):
         pass
     def abrirRelatorio(self):
@@ -39,6 +70,6 @@ class User:
 
 if __name__ == "__main__":
     carlos = User()
-    carlos.adcionaMaterial('batata', 75)
+    #carlos.adcionaMaterial('batata', 75)
 
-    carlos.editarMaterial(1, "maca do amor", 15)
+    #carlos.editarMaterial(1, "maca do amor", 15)
